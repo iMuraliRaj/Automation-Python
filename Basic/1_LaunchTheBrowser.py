b70c86122d59511b3baadd0e0a7ad6ac55612c37
ate186 <murali.r@agaramtech.com>
2022-04-11 19:48:24 +0530

    #2
    Procedure to mention the driver path

--- Basic/1_LaunchTheBrowser.py
+++ Basic/1_LaunchTheBrowser.py
@@ -10,6 +10,7 @@ def launchBrowser():
     # 1. Calling the chrome method to launch the chrome browser
     # 2. Need to pass the chrome driver path as the argument to the Chrome method
     # 3. Need to store the Launched chrome driver as driver variable to use the launched driver in future steps
+    # 4. We can use the driver from it's project itself. So  that we need to use (..\) in driver path.
     driver = webdriver.Chrome(executable_path="..\chromedriver.exe")
 
 
