9e264af502ab4a796e21dac433056b62ec06e4dc
ate186 <murali.r@agaramtech.com>
2022-04-11 19:38:45 +0530

    Launch the browser

---
+++ Basic/1_LaunchTheBrowser.py
@@ -0,0 +1,17 @@
+# Author - R.Murali
+# Created Date - 11th April 2022
+import time
+
+from selenium import webdriver
+
+# This method is used to launch the chrome browser
+def launchBrowser():
+
+    # 1. Calling the chrome method to launch the chrome browser
+    # 2. Need to pass the chrome driver path as the argument to the Chrome method
+    # 3. Need to store the Launched chrome driver as driver variable to use the launched driver in future steps
+    driver = webdriver.Chrome(executable_path="..\chromedriver.exe")
+
+
+# Calling the Launch Browser method, Because method only runs when it is called.
+launchBrowser()
