31c66aa5c129c064cca0d9f8734d9f413e6d528a
ate186 <murali.r@agaramtech.com>
2022-04-12 10:33:28 +0530

    #7
    All method in single python

--- Basic/Basic.py
+++ Basic/Basic.py
@@ -7,15 +7,35 @@ from selenium import webdriver
 # This method is used to launch the chrome browser
 def launchBrowser():
 
+    global driver
+
     # 1. Calling the chrome method to launch the chrome browser
     # 2. Need to pass the chrome driver path as the argument to the Chrome method
     # 3. Need to store the Launched chrome driver as driver variable to use the launched driver in future steps
     # 4. We can use the driver from it's project itself. So  that we need to use (..\) in driver path.
     driver = webdriver.Chrome(executable_path="..\chromedriver.exe")
 
-    # It is used to Maximize the lauched browser
+
+
+def maximizeWindow():
+
+    # We can maximize the driver in 2 ways
+    # 1. fullscreen_window()
+    # 2. maximize_window()
+    # line(26-28)
+    driver.fullscreen_window()
+
     driver.maximize_window()
 
 
-# Calling the Launch Browser method, Because method only runs when it is called.
-launchBrowser()
+
+
+
+
+def basic():
+    launchBrowser()
+    maximizeWindow()
+
+
+# Calling the basic method, Because method only runs when it is called.
+basic()
